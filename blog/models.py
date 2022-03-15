e1b2ccf16057ae1113d62905b74f9fcbc35f0bd7
Ronald <ron_jusuf@yahoo.com>
2022-03-15 22:38:28 +1100

    admin

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Post(models.Model):
+    STATUS = ((0, 'Draft'), (1, 'Published'))
+
     title = models.CharField(max_length=200)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
