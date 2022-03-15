10aff57b726e8485776a5ee3b29a911da2e8b427
Ronald Jusuf <ronald.jusuf@seertechsolutions.com>
2022-03-15 13:24:20 +1100

    urls

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-STATUS = ((0,'Draft'), (1,'Published'))
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Post(models.Model):
