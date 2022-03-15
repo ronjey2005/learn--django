7b9ad22ca35d3381e42b981e82dc04f1e37552e6
Ronald Jusuf <ronald.jusuf@seertechsolutions.com>
2022-03-15 13:03:07 +1100

    urls

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post
+from django.views import generic
 
 # Create your views here.
-class BlogView:
+class BlogView(generic.DetailView):
     model = Post
     templates = "blog.html"
