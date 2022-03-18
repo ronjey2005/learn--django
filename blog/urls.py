2af0a79325ff935207c7ff31330438a380df4625
Ronald Jusuf <ronald.jusuf@seertechsolutions.com>
2022-03-18 17:25:42 +1100

    ..

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('<slug:slug>', views.BlogView.as_view(), name="blog_view"),
-    path('', views.HomeView.as_view(), name="home_view")
+    path('home/', views.HomeView.as_view(), name="home_view")
 ]
