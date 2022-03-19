7d3f6df217c18648136d829e8e3c576e25bec9e8
Ronald Jusuf <ronald.jusuf@seertechsolutions.com>
2022-03-19 22:38:11 +1100

    bloglist

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,14 @@ from django.views import generic
 
 # Create your views here.
 
-
 class BlogView(generic.DetailView):
     model = Post
     template_name = "blog.html"
 
-
 class HomeView(generic.TemplateView):
     template_name = "home.html"
-
 
 class BlogListView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('date_created')
     template_name = "index.html"
+    context_object_name = "blog_list_view"
