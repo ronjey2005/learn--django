91ce4ff5dde2dd3c5381dae9a058f3778037be8f
Ronald <ron_jusuf@yahoo.com>
2022-03-26 12:32:43 +1100

    translator views

--- translator/views.py
+++ translator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+#from .models import Post
+from django.views import generic
 
 # Create your views here.
+class TranslatorView(generic.TemplateView):
+    template_name = 'templates/translator.html'
+    context_object_name = "translator_view"
+
+
+
