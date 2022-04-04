e0ad3b5264ab30b2c3c54985028fb8b4572bc92a
Ronald Jusuf <ronald.jusuf@seertechsolutions.com>
2022-04-04 17:49:19 +1000

    Languages

--- translator/views.py
+++ translator/views.py
@@ -8,18 +8,12 @@ def TranslatorView(request):
         original_text = request.POST['my_textarea']
         #translated_text = original_text.upper()
         translated_text = translate_input(original_text, 'de')
-        context = {"original_text": original_text, "translated_text": translated_text}
+        context = {"original_text": original_text, "translated_text": translated_text, "lang_codes": LANGUAGES}
         return render(request,'templates/translator.html', context)
     else:
         #original_text = request.POST['my_textarea']
-        context = {"original_text": "", "translated_text": ""}
+        context = {"original_text": "", "translated_text": "", "lang_codes": LANGUAGES}
         return render(request, 'templates/translator.html', context)
 
-def LangList():
-    context = {"LANG_CODES": LANGUAGES}
-    print(context)
-    return context
-
-LangList()
 
 
