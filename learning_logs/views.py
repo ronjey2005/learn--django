69ce90bdb802524d8aac80a806a07b43fff43505
Ronald <ron_jusuf@yahoo.com>
2022-04-18 18:06:56 +1000

    learning logs

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 def topicsListView(request):
 
-    context['some_data'] = 'This is just some data'
+    context = {
+        "dummy_context": "hello"
+    }
     return render(request,'templates/learning_logs/topics-list.html', context)
