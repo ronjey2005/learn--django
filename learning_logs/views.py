16d6e637d9d4781988a1f2f22658cef4885ab8fb
Ronald <ron_jusuf@yahoo.com>
2022-04-19 22:32:48 +1000

    topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .models import Topic
 
 def topicsListView(request):
+
+    topics = Topic
+
+    print(dir(topics.objects))
 
     context = {
         "dummy_context": "hello"
