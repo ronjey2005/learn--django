e9a5098db091b2e58fae178e60bc58bdfb3d2111
Ronald <ron_jusuf@yahoo.com>
2022-04-26 22:19:41 +1000

    QUERIES

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 
 def topicsListView(request):
 
     topics = Topic
-
+    entries = Entry
     context = {
-        "titles": topics.objects.all()
+        "titles": [],
+        "entries": []
     }
 
-    print(context)
+    for topic in topics.objects.all():
+        context['titles'].append(topics.objects.get(id=topic.id))
+        context['entries'].append(entries.objects.get(topic_id=topic.id))
+        print(entries.objects(topic_id=topic.id))
+
+    #print(context['entries'])
+
+
 
     return render(request,'templates/learning_logs/topics.html', context)
+
+# def entryView(request):
+#
+#     entry = Entry
+#
+#     context = {
+#
+#     }
