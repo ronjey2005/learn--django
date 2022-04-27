329e55938b1234c86aedda6e88a98f40b7c0e098
Ronald <ron_jusuf@yahoo.com>
2022-04-28 08:15:18 +1000

    QUERIES

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,7 +4,9 @@ from .models import Topic, Entry
 def topicsListView(request):
 
     topics = Topic
+
     entries = Entry
+
     context = {
         "titles": [],
         "entries": []
@@ -13,11 +15,9 @@ def topicsListView(request):
     for topic in topics.objects.all():
         context['titles'].append(topics.objects.get(id=topic.id))
         context['entries'].append(entries.objects.get(topic_id=topic.id))
-        print(entries.objects(topic_id=topic.id))
+        #print(topic.id)
 
-    #print(context['entries'])
-
-
+    print(context['entries'])
 
     return render(request,'templates/learning_logs/topics.html', context)
 
